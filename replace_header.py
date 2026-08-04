73825f677774e047b6f1794ce8aecbcc592540b3
itistudycentre <er.yadavamaresh@gmail.com>
2026-08-04 08:21:37 +0530

    Update replace_header.py

--- replace_header.py
+++ replace_header.py
@@ -1,22 +1,15 @@
 import os
 import re
 
-new_header = '''
-<div id="header-placeholder"></div>
-
+NEW_HEADER = """<div id="header-placeholder"></div>
 <script>
-fetch("/header.html")
-.then(r=>r.text())
-.then(html=>{
-document.getElementById("header-placeholder").innerHTML=html;
+fetch('/header.html')
+.then(r => r.text())
+.then(html => {
+    document.getElementById('header-placeholder').innerHTML = html;
 });
 </script>
-'''
-
-start = r'<!-- =========================================\s*HEADER.*?========================================= -->'
-end = r'<!-- ===========================================\s*HERO'
-
-pattern = re.compile(start + r'[\s\S]*?' + end, re.MULTILINE)
+"""
 
 for root, dirs, files in os.walk("."):
     if ".git" in root:
@@ -25,15 +18,18 @@ for root, dirs, files in os.walk("."):
     for file in files:
         if file.endswith(".html"):
 
-            path = os.path.join(root,file)
+            path = os.path.join(root, file)
 
-            with open(path,"r",encoding="utf-8") as f:
-                txt=f.read()
+            with open(path, "r", encoding="utf-8") as f:
+                html = f.read()
 
-            txt2 = pattern.sub(new_header+"\n\n<!-- ===========================================\n    HERO",txt)
+            pattern = r'<div class="main-header".*?<section class="hero-banner">'
+            repl = NEW_HEADER + "\n\n<section class=\"hero-banner\">"
 
-            if txt!=txt2:
-                with open(path,"w",encoding="utf-8") as f:
-                    f.write(txt2)
+            new_html = re.sub(pattern, repl, html, flags=re.S)
 
-                print(path)
+            if new_html != html:
+                with open(path, "w", encoding="utf-8") as f:
+                    f.write(new_html)
+
+                print("Updated:", path)
